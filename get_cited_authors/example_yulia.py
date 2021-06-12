f6143c1da77301bb5158417f251bc455ee271755
kendalfoster <kendal.foster@warwick.ac.uk>
2021-06-12 10:29:17 -0400

    added counts of each individual author

---
+++ get_cited_authors/example_yulia.py
@@ -0,0 +1,4 @@
+from get_cited_authors import *
+
+yulia_ca = get_cited_authors("yulia.txt")
+print(yulia_ca)
--- get_cited_authors/get_cited_authors.py
+++ get_cited_authors/get_cited_authors.py
@@ -1,5 +1,7 @@
 def get_cited_authors(text_file):
-	"""parses the Web of Science text_file for the Cited References section and returns a list of authors (unsorted, in various capitalizations, containing duplicates)"""
+	"""parses the Web of Science text_file for the Cited References section and returns a dictionary list of authors with counts for each author (sorted by number of occurrences, all uppercase)"""
+	
+	from collections import Counter
 	
 	cited_authors  = []
 	with open (text_file, 'rt') as citefile:
@@ -7,13 +9,11 @@ def get_cited_authors(text_file):
 		for line in citefile:
 			if cr_flag:
 				if line[:2] == "  ":
-					cited_authors.append(line.split(",")[0][3:])
+					cited_authors.append(line.split(",")[0][3:].upper())
 				else:
 					cr_flag = False
 			elif line[:2] == "CR":
 				cr_flag = True
-				cited_authors.append(line.split(",")[0][3:])
-	return cited_authors
-
-yulia_cr = get_cited_authors("sample.txt")
-print(yulia_cr)
+				cited_authors.append(line.split(",")[0][3:].upper())
+	author_count = Counter(cited_authors)
+	return author_count
